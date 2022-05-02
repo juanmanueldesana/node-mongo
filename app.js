14bd8ed29a51569528490799c4e395a31dde9c16
Juan Manuel <juanmanuel.manuel70@gmail.com>
2022-05-01 21:55:50 -0300

    model user creado

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dbConnect = require('./config/mongo')
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
+
+dbConnect()
---
+++ models/nosql/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserScheme = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamp: true, //adds createdAt and updatedAt fields
+    versionKey: false,
+  }
+);
+
+
+//se define el modelo de datos para la coleccion de usuarios
+module.exports = mongoose.model("users", UserScheme);
